64444550d941b04cd62b23470956a10182f9d18d
bekturinho <ismanalievbektur4@gmail.com>
2024-06-26 18:49:57 -0500

    Initial Commit

---
+++ BMIcalculatorSwiftUi/BMIcalculatorSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIcalculatorSwiftUiApp.swift
+//  BMIcalculatorSwiftUi
+//
+//  Created by fortune cookie on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct BMIcalculatorSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
